90bb5f2c8a1c61620fc078ee1644028ecc20beca
iliasradouche <iliasradouche2@gmail.com>
2024-04-03 15:16:46 +0000

    Changing link for Download Button v-4

--- src/components/DownloadMyPdf.jsx
+++ src/components/DownloadMyPdf.jsx
@@ -12,7 +12,7 @@ const container = (delay) => ({
 const DownloadPDFButton = () => {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "https://drive.google.com/file/d/10eyHQi6vIn-E3cRW2t47eNeCqSDIA8sg/view?usp=drive_link";
+    link.href = "https://drive.usercontent.google.com/download?id=10eyHQi6vIn-E3cRW2t47eNeCqSDIA8sg&export=download&authuser=0&confirm=t&uuid=388f65fc-631b-4697-ab08-d13f70addc1e&at=APZUnTXZMzENqtsj2wG1cc5WE0ub:1712157371757";
     link.download = "ILIAS_RADOUCHE_CV.pdf";
     document.body.appendChild(link);
     link.click();
